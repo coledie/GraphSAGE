59cdc436100ea3328eec7fbbe3b0475ad66c5015
csdhv9 <csdhv9@umsystem.edu>
2021-02-11 01:26:49 -0600

    Add usage docs.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # GraphSAGE
 GraphSAGE + Control variate for GCN implemented in PyTorch.
+
+## Usage
+
+All use the PPI dataset by default.
+
+Run the desired file as,
+```bash
+# Supervised graphsage
+python graphsage.py
+
+# Unsupervised graphsage
+python graphsage_unsup.py
+
+# Control variate on supervised graphsage
+python cv.py
+
+# Control variate on unsupervised graphsage
+python cv.py --unsup
+```
--- cv.py
+++ cv.py
@@ -2,10 +2,23 @@
 Wrapper that applies control variate, variance reduction
 to graph convolutional neural networks.
 
+For supervised graphsage,
+```bash
+python graphsage.py
+```
+
+For unsupervised graphsage,
+```bash
+python cv.py --unsup
+```
+
 https://arxiv.org/abs/1710.10568
 """
 
 
 if __name__ == '__main__':
     from graphsage import GraphSAGE
+
     model = cv_wrapper(GraphSAGE())
+
+    # add parser arg --unsup for unsueprvised graphsage
